e3110e08129eaa6e55a4f56d2a3efa8634347e00
Liliia <liliia.titova200699@gmail.com>
2022-08-06 17:32:18 +0300

    ex4hometask less1

---
+++ ex4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задание 4
+Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if (secondNumber > max) max = secondNumber;
+if (thirdNumber > max) max = thirdNumber;
+Console.Write("max=");
+Console.WriteLine(max);
