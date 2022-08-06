1e95463e71b5b9751530d9ac23fda707b12ef2a1
Liliia <liliia.titova200699@gmail.com>
2022-08-06 18:05:43 +0300

    ex6hometask_less1

---
+++ ex6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задание 6
+Console.WriteLine("Введите число:");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 ==0) 
+{
+    Console.WriteLine ("число чётное");
+}
+else 
+{
+    Console.WriteLine("число нечётное");
+}
