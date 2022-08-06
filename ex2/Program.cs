49539cb5ad76c5264dba5d06b3ff799b776211f8
Liliia <liliia.titova200699@gmail.com>
2022-08-06 16:23:33 +0300

    ex2homework less1

---
+++ ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задание 2
+Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine( firstNumber + " " + "большее число");
+    Console.WriteLine( secondNumber + " " + "меньшее");
+}
+else 
+{
+    Console.WriteLine( secondNumber + " " + "большее число");
+    Console.WriteLine( firstNumber + " " + "меньшее");
+}
