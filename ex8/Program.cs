7734946aa8ef7990743d19b9284e819acf941120
Liliia <liliia.titova200699@gmail.com>
2022-08-06 19:17:23 +0300

    ex8homework_less1

---
+++ ex8/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задание 8
+Console.WriteLine("Введите число:");
+int Number = Convert.ToInt32(Console.ReadLine());
+int startNumber = 2;
+while(startNumber <= Number)
+{
+    Console.Write ( startNumber + " ");
+    startNumber = startNumber+2;
+}
